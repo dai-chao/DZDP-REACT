550fc2de18b2891b0f28db8e81a9d6aeeeab2d86
dai-chao <1176227442@qq.com>
2019-05-17 16:45:29 +0800

    第二次

---
+++ src/component/download/index.js
@@ -0,0 +1,24 @@
+
+import React,{Component} from "react"
+
+export default class Download extends Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return(
+            <div>
+                <div></div>
+            </div>
+        )
+    }
+}
+
+
+
+
+
+
+
+
+
